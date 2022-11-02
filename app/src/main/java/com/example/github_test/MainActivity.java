8b9f9cf61aa5635247c5fb4fda2f87bcfc681f4c
GomtanG2 <qazwsx636@naver.com>
2022-11-02 17:48:37 +0900

    git init

---
+++ app/src/main/java/com/example/github_test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.github_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    int i1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
