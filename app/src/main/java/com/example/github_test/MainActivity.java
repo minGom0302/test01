51a2894bcfc6b9481df5e12dcac65b4d33a7abf7
GomtanG2 <qazwsx636@naver.com>
2022-11-04 14:22:09 +0900

    git commit -m "update test01"

--- app/src/main/java/com/example/github_test/MainActivity.java
+++ app/src/main/java/com/example/github_test/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.github_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int i1;
+    TextView tv1, tv2, tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = findViewById(R.id.textView1);
+        tv2 = findViewById(R.id.textView2);
+        tv3 = findViewById(R.id.textView3);
+
+        tv1.setText("나도");
+        tv2.setText("놀고싶다");
+        tv3.setText("!!");
     }
 }
